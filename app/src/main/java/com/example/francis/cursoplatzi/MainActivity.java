028e233854a9fda248cfebac078b396982768083
Francis Alonso Gonzalez Zarate <francis_gz@hotmail.com>
2015-07-30 15:54:36 -0500

    Se agregaron los fragments y se corrigio el error de action bar

--- app/src/main/java/com/example/francis/cursoplatzi/MainActivity.java
+++ app/src/main/java/com/example/francis/cursoplatzi/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.francis.cursoplatzi;
 
 import android.content.Intent;
 import android.graphics.Outline;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
@@ -16,24 +18,27 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         /*int size = getResources().getDimensionPixelSize(R.dimen.fab_size);
         Outline outline = new Outline();
-
-        outline.setOval(0,0,size,size);
+        outline.setOval(0,0,size,size);*/
         ImageButton imageButton = (ImageButton) findViewById(R.id.fab_1);
-        imageButton.setOutline(outline);*/
+      //  imageButton.setOutline(outline);
 
 
     }
 
 
     public void onClick(View v){
+
+
         Toast.makeText(this,"Hola estoy escuchando",Toast.LENGTH_LONG).show();
 
         Intent intent = new Intent(this,MyMusic.class);
         startActivity(intent);
+
     }
 
 
--- app/src/main/java/com/example/francis/cursoplatzi/MyMusic.java
+++ app/src/main/java/com/example/francis/cursoplatzi/MyMusic.java
@@ -1,20 +1,44 @@
 package com.example.francis.cursoplatzi;
 
+//import android.app.ActionBar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 
-public class MyMusic extends ActionBarActivity {
+public class MyMusic extends ActionBarActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_my_music);
+
+
+        setTabs();
+
+
+
+
     }
 
-    @Override
+    private void setTabs() {
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+
+       /* actionBar.addTab(actionBar.newTab().setText("Destacados").setTabListener(this));
+        actionBar.addTab(actionBar.newTab().setText("Amigos").setTabListener(this));
+        actionBar.addTab(actionBar.newTab().setText("Perfil").setTabListener(this));*/
+    }
+
+
+
+   /* @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_my_music, menu);
@@ -34,5 +58,5 @@ public class MyMusic extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 }
